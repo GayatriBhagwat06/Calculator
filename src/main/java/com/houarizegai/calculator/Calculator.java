7bf6195ddbf8afc1c1d4ee21f19c774ae637e005
GayatriBhagwat06 <88124547+GayatriBhagwat06@users.noreply.github.com>
2022-08-26 13:07:07 +0530

    AB#14

--- src/main/java/com/houarizegai/calculator/Calculator.java
+++ src/main/java/com/houarizegai/calculator/Calculator.java
@@ -623,7 +623,7 @@ public class Calculator {
     public static void main(String[] args) {
         new Calculator();
         System.out.print("Hello World");
-        System.out.print("US13);
+        System.out.print("US14);
 
     }
 }
